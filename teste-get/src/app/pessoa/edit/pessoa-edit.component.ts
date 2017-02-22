635c2a146b92d530528c75513c2e947f927d47e4
wagner <wagner@wagner-Infoway>
2017-02-21 21:13:25 -0300

    ajustes----

--- teste-get/src/app/pessoa/edit/pessoa-edit.component.ts
+++ teste-get/src/app/pessoa/edit/pessoa-edit.component.ts
@@ -4,7 +4,7 @@ import { PessoaService } from 'app/service/pessoa.service';
 import { ITdDataTableColumn } from '@covalent/core'
 
 @Component({
-  selector: 'app-pessoa',
+  selector: 'app-pessoa-list',
   templateUrl:  './pessoa-edit.component.html',
   styleUrls: ['./pessoa-edit.component.css']
 })
@@ -19,5 +19,18 @@ export class PessoaEditComponent implements OnInit {
   constructor(private pessoaService: PessoaService) {
     this.pessoaService.getPessoas()
       .subscribe(data => this.pessoas = data);
+  }
+
+  deletePessoa(pessoa) {
+    if (confirm("Você quer deletar " + pessoa.nome + "?")) {
+      var index = this.pessoas.indexOf(pessoa);
+      this.pessoas.splice(index, 1);
+
+      this.pessoaService.deletePessoa(pessoa.id)
+        .subscribe(null, err => {
+          alert("Não foi possivel deletar");
+          this.pessoas.splice(index, 0, pessoa);
+        });
+    }
   }
 }
--- teste-get/src/app/pessoa/list/pessoa-list.component.ts
+++ teste-get/src/app/pessoa/list/pessoa-list.component.ts
@@ -4,7 +4,7 @@ import { PessoaService } from 'app/service/pessoa.service';
 import { ITdDataTableColumn } from '@covalent/core'
 
 @Component({
-  selector: 'app-pessoa',
+  selector: 'app-pessoa-list',
   templateUrl:  './pessoa-list.component.html',
   styleUrls: ['./pessoa-list.component.css']
 })
@@ -20,4 +20,22 @@ export class PessoaListComponent implements OnInit {
     this.pessoaService.getPessoas()
       .subscribe(data => this.pessoas = data);
   }
+  deletePessoa(pessoa) {
+    if (confirm("Você quer deletar " + pessoa.nome + "?")) {
+      var index = this.pessoas.indexOf(pessoa);
+      this.pessoas.splice(index, 1);
+
+      this.pessoaService.deletePessoa(pessoa.id)
+        .subscribe(null, err => {
+          alert("Não foi possivel deletar");
+          this.pessoas.splice(index, 0, pessoa);
+        });
+    }
+  }
+
+  private columns: ITdDataTableColumn[] = [
+    { name: 'nome', label: 'Nome' },
+    { name: 'tipo', label: 'Tipo'}
+  ];
+
 }
