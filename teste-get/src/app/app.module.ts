7aec22db89353190797f3fc83d0a46c397e53654
wagner <wagner@rpawsds09.redetre.jus.br>
2017-02-22 10:26:50 -0300

    Redirect usando o router funcionando......

--- teste-get/src/app/app.module.ts
+++ teste-get/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'
 import { CovalentCoreModule, CovalentLayoutModule, CovalentDataTableModule } from '@covalent/core'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PessoaComponent } from './pessoa/pessoa.component';
 import { PessoaService } from './service/pessoa.service';
 import { PessoaEditComponent } from "./pessoa/edit/pessoa-edit.component";
 import { PessoaListComponent } from "./pessoa/list/pessoa-list.component";
-
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -26,9 +27,10 @@ import { PessoaListComponent } from "./pessoa/list/pessoa-list.component";
     MaterialModule.forRoot(),
     CovalentCoreModule.forRoot(),
     CovalentLayoutModule.forRoot(),
-    CovalentDataTableModule.forRoot()
+    CovalentDataTableModule.forRoot(),
+    AppRoutingModule
   ],
-  providers: [PessoaService],
+  providers: [PessoaService, AppRoutingModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
