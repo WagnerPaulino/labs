03c34f57cf41df8beed23b6848cef9129a76411c
wagner <wagner@wagner-Infoway>
2017-03-11 14:10:43 -0300

    renomeação do crud rest

---
+++ crud-rest-angular-2/src/app/pessoa/edit/pessoa-edit.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Pessoa } from 'app/domain/pessoa';
+import { PessoaService } from 'app/service/pessoa.service';
+import { ITdDataTableColumn } from '@covalent/core';
+import { NgForm, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { ITdDynamicElementConfig, TdDynamicElement, TdDynamicType } from '@covalent/dynamic-forms';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-pessoa-list',
+  templateUrl:  './pessoa-edit.component.html',
+  styleUrls: ['./pessoa-edit.component.css']
+})
+export class PessoaEditComponent implements OnInit, OnDestroy {
+
+  private pessoa: Pessoa = new Pessoa();
+  
+  ngOnInit() {
+    this.route.params
+    .map(params => params['id'])
+    .switchMap(id => this.pessoaService.getPessoa(id))
+    .subscribe(pessoa => this.pessoa = pessoa);
+  }
+
+  constructor(private pessoaService: PessoaService, private route: ActivatedRoute) {
+    //this.pessoaService.getPessoas();
+     // .subscribe(data => this.pessoa = data);
+  }
+
+  save(form: NgForm){
+    
+    if(form.value.id == null){
+    this.pessoaService.addPessoa(this.pessoa).subscribe(form.value);
+    console.log("executando insert");
+  }else{
+  this.pessoaService.updatePessoa(this.pessoa).subscribe(form.value);
+    console.log("executando update");
+  }
+  console.log(form.value);
+    console.log(this.pessoa);
+  }
+
+  deletePessoa(pessoa) {
+      this.pessoaService.deletePessoa(pessoa.id);
+  }
+
+  ngOnDestroy(){
+    this.pessoa = new Pessoa();
+  }
+
+/*elements: ITdDynamicElementConfig[] = [{
+    name: 'nome',
+    type: TdDynamicElement.Input,
+    required: false
+  },
+  {
+    name: 'tipo',
+    type: TdDynamicElement.Input,
+    required: false
+  }
+];*/
+
+}
