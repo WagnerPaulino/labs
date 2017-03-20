63ee22be674939bf5f36f1aa5e89fcabdb1ca37f
wagner <wagner@rpawsds09.redetre.jus.br>
2017-03-20 11:48:41 -0300

    primeiro commit

---
+++ springboot-angular2-tddatatable-paging/src/app/view/mngt/@dashboard/mngt-dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn } from '@covalent/core';
+import { IPageChangeEvent } from '@covalent/core';
+import { MessageService } from 'app/service/message.service';
+
+@Component({
+  selector: 'mngt-dashboard',
+  templateUrl: './mngt-dashboard.component.html',
+  styleUrls: ['./mngt-dashboard.component.css']
+})
+export class MngtDashboardComponent implements OnInit {
+
+ data: any[] = [];
+  columns: ITdDataTableColumn[] = [
+    { name: 'message', label: 'Messagem' },
+    { name: 'escritor', label: 'Escritor' }
+  ];
+filteredData: any[] = this.data;
+ 
+
+  constructor(private _dataTableService: TdDataTableService, private messageService: MessageService) {
+    this.data = this.messageService.getMessages();
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+  
+}
