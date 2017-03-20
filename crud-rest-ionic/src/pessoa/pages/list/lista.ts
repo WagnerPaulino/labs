e7063b51c3a5c47c0b94c634cef816894c7d97f4
wagner <wagner@wagner-Infoway>
2017-03-20 20:11:06 -0300

    Insert feito

--- crud-rest-ionic/src/pessoa/pages/list/lista.ts
+++ crud-rest-ionic/src/pessoa/pages/list/lista.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Pessoa } from "../../../domain/pessoa";
+import { PessoaService } from "../../../services/pessoaService";
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'lista.html'
 })
 export class ListaPage {
+  private pessoas : Pessoa[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, pessoaService: PessoaService) {
+    pessoaService.getPessoas().subscribe(data => this.pessoas = data);
   }
 
 }
