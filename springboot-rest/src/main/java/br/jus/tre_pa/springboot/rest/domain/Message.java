f67ef842a62da74b9420e81b8be5a6a1ae479812
wagner <wagner@rpawsds09.redetre.jus.br>
2017-03-24 11:50:44 -0300

    adição de um atributo byte no domain

--- springboot-rest/src/main/java/br/jus/tre_pa/springboot/rest/domain/Message.java
+++ springboot-rest/src/main/java/br/jus/tre_pa/springboot/rest/domain/Message.java
@@ -1,11 +1,15 @@
 package br.jus.tre_pa.springboot.rest.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.mysql.jdbc.Blob;
 
 @Entity
 @Table(name = "message")
@@ -22,6 +26,9 @@ public class Message {
 
 	@Column(unique=true)
 	private String message;
+	
+	@Column
+	private Byte arquivo;
 	
 	@Column
 	private String escritor;
