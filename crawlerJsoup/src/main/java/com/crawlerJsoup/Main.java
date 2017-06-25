b3c81307c0bbb8f4be768eb885795f93710e620a
wagner <wagner@wagner-Infoway>
2017-06-25 10:03:56 -0300

    crawler...

--- crawlerJsoup/src/main/java/com/crawlerJsoup/Main.java
+++ crawlerJsoup/src/main/java/com/crawlerJsoup/Main.java
@@ -10,8 +10,9 @@ public class Main {
 	private static String user = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
 
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("http://www.google.com").userAgent(user).get();
-		System.out.println(doc);
+		Document doc = Jsoup.connect("http://www.google.com.br/search?gcx=c&ix=c1&sourceid=chrome&ie=UTF-8&q=teste").userAgent(user).get();
+		String resul = doc.select("div.g").text();
+		System.out.println(resul);
 	}
 
 }
