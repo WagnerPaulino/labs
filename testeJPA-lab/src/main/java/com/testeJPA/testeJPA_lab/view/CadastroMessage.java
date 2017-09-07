28e999dd69b4cf3bf8ece3cba3fee59c4bb8819d
wagner <wagner@wagner-Infoway>
2017-09-07 09:09:58 -0300

    Adição do butão selecionar

--- testeJPA-lab/src/main/java/com/testeJPA/testeJPA_lab/view/CadastroMessage.java
+++ testeJPA-lab/src/main/java/com/testeJPA/testeJPA_lab/view/CadastroMessage.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JButton;
@@ -24,12 +25,13 @@ public class CadastroMessage {
 
 	private JFrame mainFrame;
 	JTextField jmessage = new JTextField();
-	JTextField imagem = new JTextField();
+	// JTextField imagem = new JTextField();
 	JTextField parametro = new JTextField();
+	JFileChooser file = new JFileChooser();
+	JButton selecionar = new JButton("Selecionar");
 	JPanel footerFrame = new JPanel();
-	
-	
-	
+	String caminhoArquivo = null;
+
 	private Message message = new Message();
 	private MessageFacade facade = new MessageFacade();
 	private Utils utils = new Utils();
@@ -45,9 +47,9 @@ public class CadastroMessage {
 		layout.setHgap(50);
 		layout.setHgap(50);
 		mainFrame = new JFrame("Java Swing Examples");
-		
+
 		footerFrame.setLayout(new GridLayout(1, 3));
-		
+
 		mainFrame.setSize(600, 600);
 		mainFrame.setLayout(layout);
 		mainFrame.add(footerFrame);
@@ -62,17 +64,17 @@ public class CadastroMessage {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					message.setMessage(jmessage.getText());
-					message.setArquivo(utils.imageToByte(imagem.getText()));
+					message.setArquivo(utils.imageToByte(caminhoArquivo));
 					facade.createMessage(message);
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		mainFrame.add(okButton);
-		
+
 		JButton jprocurar = new JButton("Procurar");
 		okButton.addActionListener(new ActionListener() {
 			@Override
@@ -82,36 +84,49 @@ public class CadastroMessage {
 				} catch (Exception e2) {
 					System.out.println(e2);
 				}
-				
-				
+
 			}
 		});
+
+		selecionar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int retorno = file.showOpenDialog(null);
+				if (retorno == JFileChooser.APPROVE_OPTION) {
+					caminhoArquivo = file.getSelectedFile().getAbsolutePath();
+				}
+
+			}
+		});
+		
+
 		footerFrame.add(jprocurar);
 
 		mainFrame.setVisible(true);
 	}
-	private void showInputs(){
+
+	private void showInputs() {
 		JLabel texto = new JLabel("Insira a sua mensagem:");
 		jmessage.setMinimumSize(new Dimension(50, 50));
-		
-		
+
 		JLabel texto2 = new JLabel("Insira a sua imagem:");
 		jmessage.setMinimumSize(new Dimension(50, 50));
-		
+
 		JLabel texto3 = new JLabel("Insira o parametro:");
 		jmessage.setMinimumSize(new Dimension(50, 50));
-		
-		
+
 		mainFrame.add(texto);
 		mainFrame.add(jmessage);
-		
+
 		mainFrame.add(texto2);
-		mainFrame.add(imagem);
 		
 		
+		mainFrame.add(selecionar);
+
 		footerFrame.add(parametro);
 		footerFrame.add(texto3);
-		
+
 		mainFrame.setVisible(true);
 	}
 
